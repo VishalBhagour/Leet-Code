ec4a1a04b5abd6664f479ef74f39fad5749a1b0f
VishalBhagour <vishalbhagour7@gmail.com>
2026-06-18 13:02:23 +0530

    Time: 3 ms (13.09%), Space: 42.1 MB (99.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 class Solution {
-    public void sortColors(int[] nums) {
-        
+    public ArrayList<Integer> sortColors(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            result.add(arr[i]);
+        }
+        return result;
     }
 }
