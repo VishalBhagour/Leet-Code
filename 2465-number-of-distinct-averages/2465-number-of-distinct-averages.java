d511a20fd854d0080ffba889cf8a55e728d59a0e
VishalBhagour <vishalbhagour7@gmail.com>
2026-09-25 11:04:07 +0530

    Time: 3 ms (94.44%), Space: 43.3 MB (20.8%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.java
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.java
@@ -3,8 +3,8 @@ class Solution {
         int n=nums.length;
         HashSet<Integer>h=new HashSet<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            int sum=(nums[i]+nums[n-1-i]);
+        for(int i=0;i<n;i++){
+            int sum=nums[i]+nums[n-1-i];
             h.add(sum);
         }
         return h.size();
